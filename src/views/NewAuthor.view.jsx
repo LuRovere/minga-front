229ff2732131162a4d40c885a85adab011303bb7
laptinho <camsanbar@gmail.com>
2023-01-04 17:47:17 -0300

    anadido primera funcionalidad de validaciones

--- src/views/NewAuthor.view.jsx
+++ src/views/NewAuthor.view.jsx
@@ -1,36 +1,69 @@
-import React, { useRef } from 'react'
+import React, { useState } from 'react'
 import { Form } from '../components'
+import validateData from '../services/validateData'
 import postData from '../services/postData'
 
 const NewAuthor = () => {
-  const inputName = useRef()
-  const inputLastName = useRef()
-  const inputAddress = useRef()
-  const inputBirthday = useRef()
-  const inputImageUrl = useRef()
+  const [inputName, setInputName] = useState('')
+  const [inputLastName, setInputLastName] = useState('')
+  const [inputAddress, setInputAddress] = useState('')
+  const [inputBirthday, setInputBirthday] = useState('')
+  const [inputImageUrl, setInputImageUrl] = useState('')
   const _handleSubmit = async (e) => {
     e.preventDefault()
-    const [city, country] = inputAddress.current.value.split(',')
-    const data = {
-      name: inputName.current.value.trim(),
-      last_name: inputLastName.current.value.trim(),
-      city,
-      country,
-      date: inputBirthday.current.value,
-      photo: inputImageUrl.current.value.trim(),
-      user_id: '63b58ab5f67ecbbe4fe5d0d8'
+    const [city, country] = inputAddress.trim().split(',')
+    const authorSchema = {
+      name: {
+        value: inputName.trim(),
+        min: 3,
+        max: 15,
+        require: true
+      },
+      last_name: {
+        value: inputLastName.trim(),
+        min: 3,
+        max: 15,
+        require: false
+      },
+      city: {
+        value: city,
+        min: 3,
+        max: 15,
+        require: true
+      },
+      country: {
+        value: country,
+        min: 3,
+        max: 15,
+        require: true
+      },
+      date: {
+        value: inputBirthday,
+        min: '1950-01-01',
+        max: '2005-01-01',
+        require: false
+      },
+      photo: {
+        value: inputImageUrl.trim(),
+        require:true
+      }
     }
-    const message = await postData('http://localhost:8080/api/authors/', data)
+    const response = validateData(authorSchema)
+    if(!response.validated) {
+      alert(response.message)
+      return false
+    }
+    const message = await postData('http://localhost:8080/api/authors/', {...response.data, user_id: '63b58ab5f67ecbbe4fe5d0d8'})
     alert(message)
   }
   return (
     <div>
       <Form handler={_handleSubmit}>
-        <input type="text" name="name" placeholder="Insert name here..." ref={inputName} />
-        <input type="text" name="last-name" placeholder="Insert last name here..." ref={inputLastName} />
-        <input type="text" name="address" placeholder="Insert address here... (city, country)" ref={inputAddress} />
-        <input type="date" name="birth" placeholder="birthday" ref={inputBirthday} />
-        <input type="text" name="image" placeholder="Insert image url here..." ref={inputImageUrl} />
+        <input type="text" name="name" placeholder="Insert name here..." value={inputName} onChange={(e) => setInputName(e.target.value)} />
+        <input type="text" name="last-name" placeholder="Insert last name here..." value={inputLastName} onChange={(e) => setInputLastName(e.target.value)} />
+        <input type="text" name="address" placeholder="Insert address here... (city, country)" value={inputAddress} onChange={(e) => setInputAddress(e.target.value)} />
+        <input type="date" name="birth" placeholder="birthday" value={inputBirthday} onChange={(e) => setInputBirthday(e.target.value)} />
+        <input type="text" name="image" placeholder="Insert image url here..." value={inputImageUrl} onChange={(e) => setInputImageUrl(e.target.value)} />
       </Form>
     </div>
   )
