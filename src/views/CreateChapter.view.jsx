04366aa3cf02792fc4a0a97ed06432eff4174acf
LucianaRovere <luciana.rovere96@gmail.com>
2023-01-04 17:07:06 -0300

    formularioTerminado

--- src/views/CreateChapter.view.jsx
+++ src/views/CreateChapter.view.jsx
@@ -4,46 +4,64 @@ import React, { useRef } from "react";
 
 const CreateChapter = () => {
 
+    const inputTitle = useRef("")
+   
+    const inputPages = useRef("")
+    const inputOrder = useRef("")
+
+    let guardarData = (e) =>{
+        e.preventDefault()
+        let data = {
+            comic_id: "63ac47d8b4db2f7baacad498",
+            title: inputTitle.current.value,
+            pages:inputPages.current.value.split(",") ,
+            order: inputOrder.current.value
+        }
+        axios.post("http://localhost:8080/api/chapters", data)
+        .then (e=>console.log(e))
+        .catch(error=>console.log(error))
+    }
+
+    
+
     return(
+
         <div className="chapter">
 
-          
-        
-         <form className="formChapter">
+         <form onSubmit={guardarData} className="formChapter">
 
          <legend className="chapterTitulo">New chapter</legend>
          
              <label htmlFor="titulo">
                     
              <input className="inputChapter"
+             ref={inputTitle}
               type="text" 
               id="titulo"
-              placeholder="ingresar titulo"
+              placeholder="ingresar titulo" 
+              
               />
              </label>
 
  
-             <label htmlFor="imagen">
-             <input className="inputChapter"
-              type="text" 
-              id="imagen"
-              placeholder="Ingresar foto"
-             
-              />
-             </label>
+           
              <label htmlFor="pages">
              <input className="inputChapter"
+              ref={inputPages}
               type="text" 
               id="pages"
               placeholder="Ingresar pages"
+             
               
               />
              </label>
              <label htmlFor="order">
              <input className="inputChapter"
+             ref={inputOrder}
               type="text" 
               id="order"
               placeholder="Ingresar order"
+              
              
               />
              </label>
