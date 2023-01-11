2b115a8fffab3f874ea94d0a48282ba248405563
laptinho <camsanbar@gmail.com>
2023-01-11 12:39:30 -0300

    Maquetacion completa

--- src/views/pagesView/Pages.view.jsx
+++ src/views/pagesView/Pages.view.jsx
@@ -4,7 +4,10 @@ import './pages.css'
 const Pages = () => {
   return (
     <div className='page-container'>
-      Pages
+      <div className='page-title'>
+        <h2>Titulo</h2>
+      </div>
+      <img className='page' src="https://storage.googleapis.com/minga/mangas/thebreaker/thebreaker_001/thebreaker_001%20(01).jpg" alt="pagina" />
     </div>
   )
 }
