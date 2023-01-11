91648a7ad6ea6633c8bc543b0d7d7264268904f6
laptinho <camsanbar@gmail.com>
2023-01-11 15:17:45 -0300

    Anadido pruebas de obtencion de datos

--- src/views/pagesView/Pages.view.jsx
+++ src/views/pagesView/Pages.view.jsx
@@ -1,14 +1,28 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 import './pages.css'
 
 const Pages = () => {
+  const [data, setData] = useState({})
+  const getData = async (url) => {
+    try {
+      const response = await axios.get(url)
+      setData(response.data.response)
+    } catch (error) {
+      console.log()
+    }
+  }
+  useEffect(() => {
+    getData('http://localhost:8080/api/chapters/63bd77a24dc6ee27758ac91d')
+    document.title = `Minga - ${data.title}`
+  },[])
   return (
     <div className='page-container'>
       <div className='page-title'>
         <h2>Titulo</h2>
       </div>
       <div className='page'>
-        <img className='page-img' src="https://storage.googleapis.com/minga/mangas/thebreaker/thebreaker_001/thebreaker_001%20(01).jpg" alt="pagina" />
+        <img className='page-img' src={data.pages[0]} alt="pagina" />
       </div>
     </div>
   )
