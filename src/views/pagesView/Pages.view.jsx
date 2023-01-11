dabf9392f43caa12165f045524ef2c86a4bbc517
laptinho <camsanbar@gmail.com>
2023-01-11 18:31:49 -0300

    TODO: modularizar el codigo. agregada funcionalidades

--- src/views/pagesView/Pages.view.jsx
+++ src/views/pagesView/Pages.view.jsx
@@ -4,25 +4,52 @@ import './pages.css'
 
 const Pages = () => {
   const [data, setData] = useState({})
+  const [actualImg, setActualImg] = useState(0)
   const getData = async (url) => {
     try {
       const response = await axios.get(url)
-      setData(response.data.response)
+      setData(response?.data?.response)
+      console.log(data)
     } catch (error) {
-      console.log()
+      console.log(error)
+    }
+  }
+  const _handlePrev = () => {
+    if(actualImg > 0) {
+      setActualImg(actualImg - 1)
+    }
+  }
+  const _handleNext = () => {
+    if(actualImg < data?.pages?.length - 1) {
+      setActualImg(actualImg + 1)
     }
   }
   useEffect(() => {
     getData('http://localhost:8080/api/chapters/63bd77a24dc6ee27758ac91d')
-    document.title = `Minga - ${data.title}`
+    document.title = `Minga - ${data?.title}`
+    
   },[])
   return (
     <div className='page-container'>
       <div className='page-title'>
-        <h2>Titulo</h2>
+        <h2>{data?.order} - {data?.title}</h2>
       </div>
       <div className='page'>
-        <img className='page-img' src={data.pages[0]} alt="pagina" />
+        <div className='buttons-container'>
+          <button onClick={_handlePrev} className='page-button'></button>
+          <button onClick={_handleNext} className='page-button'></button>
+        </div>
+        {
+          data?.pages?.map((page, index) => {
+            return <div key={index}>
+              {
+                actualImg === index && (
+                  <img className='page-img' src={page} alt="pagina" />
+                )
+              }
+            </div>
+          })
+        }
       </div>
     </div>
   )
