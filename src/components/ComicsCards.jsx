7428b7892b83cfc49171b4792903da977b546524
LucianaRovere <luciana.rovere96@gmail.com>
2023-01-12 11:33:39 -0300

    render de cartasComics

--- src/components/ComicsCards.jsx
+++ src/components/ComicsCards.jsx
@@ -1,20 +1,42 @@
-import React from "react";
+import axios from "axios";
+import React, {useEffect,useState} from "react";
+
 
 const ComicsCards=() =>{
+    const [cards,setCards] = useState([])
+
+    const getData = async()=>{
+        try {
+            const response = await axios.get('http://localhost:8080/api/comics')
+            setCards(response.data.response)
+          
+        }catch(err){
+            console.log(err)
+        }
+    }
+    useEffect(()=>{
+        getData()
+    }
+    ,[])
     return (
-        <div className="card">
+        <>
+            {
+                cards.map((card,index)=>{return<div key={index} className="card">
+ 
+                <div className="textoCard">
+                               <h2 className="tituloCard">{card.title}</h2>
+                               <p className="pCard">{card.category_id}</p>
+                           </div>
+               
+                           <div className="divCardImg">
+                               <img className="cardIMG" src={card.photo} alt="" />
+                           </div>
+                       </div>})
+            }
+           
             
-            <div className="textoCard">
-                <h2 className="tituloCard">Superman Comic</h2>
-                <p className="pCard">Type</p>
-            </div>
 
-            <div className="divCardImg">
-                <img className="cardIMG" src="./assets/ejemploCard.png" alt="" />
-            </div>
-            
-
-         </div>
+           </>
     )
 }
 export default ComicsCards
